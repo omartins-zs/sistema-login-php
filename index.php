ae1978f79d42b46d08497ebbb0739a017c48f492
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-16 18:26:16 -0300

    Estilizando Projeto

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start()
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bulma.min.css">
     <title>Login</title>
 </head>
 
--- login.php
+++ login.php
@@ -1,5 +1,25 @@
-<form action="" method="POST">
-    <input type="text" name="login">
-    <input type="password" name="senha">
-    <input type="submit" name="action" value="Enviar">
-</form>
+<div class="container">
+
+    <form class="control" action="" method="POST">
+        <div class="field">
+            <label class="label">Nome</label>
+            <div class="control">
+                <input class="input" type="text" name="login" placeholder="Digite seu nome">
+            </div>
+        </div>
+
+        <div class="field">
+            <p class="control has-icons-left">
+                <input class="input" type="password" name="senha" placeholder="Digite sua senha">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                </span>
+            </p>
+        </div>
+
+
+        <!-- <input type="password" name="senha"> -->
+        <input type="submit" name="action" value="Enviar">
+    </form>
+
+</div>
