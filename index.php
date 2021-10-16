935e8e5c3afc92ebac279255d3e865ab5ba07b64
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-16 17:57:17 -0300

    Adicionando Logica,Validaçoes e Condicoes

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+session_start()
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <?php
+
+    if (!isset($_SESSION['login'])) {
+        if (isset($_POST['action'])) {
+            $login = 'gabriel';
+            $senha = '123456';
+
+            $loginForm = $_POST['login'];
+            $senhaForm = $_POST['senha'];
+
+            if ($login == $loginForm && $senha == $senhaForm) {
+                // Logado com sucesso
+                $_SESSION['login'] = $login;
+                header('Location: index.php');
+            } else {
+                // Algum erro ocorreu ou Dados Errado
+                echo 'Dados invalidos';
+            }
+        }
+
+        include('login.php');
+    } else {
+        if (isset($_GET['logout'])) {
+            unset($_SESSION['login']);
+            session_destroy();
+            header('Location:index.php');
+        }
+        include('home.php');
+    }
+
+    ?>
+
+</body>
+
+</html>
